13e3b41258328e036c8468c7733bea270a0dd4de
Siddiqui-2583 <nurnaby83@gmail.com>
2020-07-20 11:07:10 +0600

    Dynamically updated components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var a = 5;
+  var style={
+    color: 'blue',
+    backgroundColor: 'white',
+  }
+  const nayoks = ['Manna', 'Riaz'];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        
+        <p className="Sum" style={style}>
+          Sum is {a + 2}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name={nayoks[0]} naika="Sabana"></Person>
+        <Person name="Omar sani" naika="Mousumi"></Person>
+        <Person name="Bappa" naika="cheka"></Person>
       </header>
+    </div>
+  );
+}
+
+function Person(props) {
+  const style = {
+    border: '2px solid white',
+    margin: '10px',
+    padding:'10px'
+  }
+  // console.log(props);
+ 
+  return (
+    <div style={style}>
+      <h2>Nayok: {props.name}</h2>
+      <p>Hero of {props.naika}</p>
     </div>
   );
 }
